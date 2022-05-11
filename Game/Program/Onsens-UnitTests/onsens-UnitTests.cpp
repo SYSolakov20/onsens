baecdd4a8e85d3f2571971e9705e1b73ab7aa141
SMTashev20 <SMTashev20@codingburgas.bg>
2022-05-11 23:37:01 +0300

    Add the base of Doxygen documentations

---
+++ Game/Program/Onsens-UnitTests/onsens-UnitTests.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../BooleoGame/Game.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace onsensUnitTests
+{
+	TEST_CLASS(onsensUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
